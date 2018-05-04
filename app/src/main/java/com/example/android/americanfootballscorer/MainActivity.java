161655abdaebe058a0d28daed5a785759eb75306
Psyborg711 <psyborg711@yahoo.com>
2018-05-04 09:53:39 -0500

    multiple corrections and updates to both the xml and java files

--- app/src/main/java/com/example/android/americanfootballscorer/MainActivity.java
+++ app/src/main/java/com/example/android/americanfootballscorer/MainActivity.java
@@ -8,10 +8,8 @@ import android.widget.TextView;
 import com.example.android.americanfootballscorer.R;
 
 public class MainActivity extends AppCompatActivity {
-
     int scoreTeamA = 0;
     int scoreTeamB = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
-
     /**
      * Displays the given score for Team A.
      */
@@ -27,52 +24,42 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
-
     public void touchdownTeamA(View v) {
         scoreTeamA = scoreTeamA + 6;
         displayForTeamA(scoreTeamA);
     }
-
     public void touchdownTeamB(View v) {
         scoreTeamB = scoreTeamB + 6;
         displayForTeamB(scoreTeamB);
     }
-
     public void fieldgoalTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
-
     public void fieldgoalTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
-
     public void conversionTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
-
     public void conversionTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
-
     public void extraPointTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
-
     public void extraPointTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
-
     public void resetScores(View v) {
         scoreTeamA = 0;
         scoreTeamB = 0;
